0bd90f5248576347d205f0fb19f2b008d4bfb77b
Brian Santos <brianmsantos81@gmail.com>
2019-04-21 20:49:56 -0700

    transition with css done

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,32 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
+                <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br><br> 
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition :name="alertAnimation">
+                <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <transition name="slide" type="animation">
+                <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <transition 
+                        appear
+                        enter-active-class="animated bounce"
+                        leave-active-class="animated shake"
+                >
+                <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <transition :name="alertAnimation" mode="out-in">
+                <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+                <div class="alert alert-warning" v-else key="warning">This is some warning</div>
 
+                </transition>
             </div>
         </div>
     </div>
@@ -13,6 +38,8 @@
     export default {
         data() {
             return {
+                show: true,
+                alertAnimation: 'fade',
 
             }
         }
@@ -20,5 +47,59 @@
 </script>
 
 <style>
+    .fade-enter {
+        opacity: 0;
+    }
+
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+
+    .fade-leave {
+
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    .slide-enter{
+        opacity: 0;
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+
+    .slide-leave {
+
+    }
+
+    .slide-leave-active{
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
+        opacity: 0;
+
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0px)
+        }
+    }
+    @keyframes slide-out {
+        to {
+            transform: translateY(0px);
+          
+        }
+        from {
+            transform: translateY(20px);
+        }
+    }
 
 </style>
